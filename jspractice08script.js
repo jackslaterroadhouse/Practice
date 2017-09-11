5bfab051327da2ebc5d3ed06f41aa7c5f22ef7ac
Jack Slater <jackslaterroadhouse@gmail.com>
2017-09-11 13:58:42 -0700

    Adding additional JS practice files

---
+++ jspractice08script.js
@@ -0,0 +1,6 @@
+			function myFunction() {
+				document.getElementById("demo").innerHTML = "Paragraph Changed.";
+			}
+			function myReturnFunction() {
+				document.getElementById("demo").innerHTML = "A Paragraph.";
+			}
